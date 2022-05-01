fc52fbb63c6ff2cb42f3e6abb844fe434048f0f0
Dhruv_Vavliya <dhruvvavliya79@gmail.com>
2022-05-01 15:50:56 +0530

    third done....wooooh

--- Problem_3/que_3.cpp
+++ Problem_3/que_3.cpp
@@ -16,10 +16,13 @@ void solve(){
     cin >> n >> p;
     
     vector<vector<lli>> a1(n ,vector<lli> (p))  ,a2(n ,vector<lli> (p));
-    vector<pair<lli, lli>> dp(2 ,make_pair(0,0));
+    vector<pair<lli, lli>> dp(2 ,make_pair(0,0));       
+
+    // pairs : { ascending best ,descending best }
+    // each pair : { last-pressure ,operations }
     
     for(int i=0 ;i<n ;i++){
-        vector<pair<lli, lli>> tmp(2 ,make_pair(LLONG_MAX,LLONG_MAX));
+        vector<pair<lli, lli>> temp(2 ,make_pair(LLONG_MAX,LLONG_MAX));
         for( int j=0 ;j<p ;j++ ) cin >> a1[i][j];
 
         sort(all(a1[i]));
@@ -27,35 +30,45 @@ void solve(){
         reverse(all(a2[i]));
 
         auto &v1=a1[i] ,&v2=a2[i];
-        for(int j=0 ;j<2 ;j++){
+
+        // consider if sorted pressures gives less operations. 
+        for(int j=0 ;j<=1 ;j++){
             lli st=dp[j].first ,sc=dp[j].second;
             for(auto &x : v1){
                 sc += abs(st-x);
                 st = x;
             }
-            if(tmp[0].second > sc){
-                tmp[0] = {st,sc};
+            if(temp[0].second > sc){
+                temp[0] = {st ,sc};
             }
         }
-        
-        for(int j=0 ;j<2 ;j++){
+
+        // consider if reverse sorted pressures gives less operations.
+        for(int j=0 ;j<=1 ;j++){
             lli st=dp[j].first ,sc=dp[j].second;
             for(auto &x : v2){
                 sc += abs(st-x);
                 st = x;
             }
-            if(tmp[1].second>sc){
-                tmp[1] = {st ,sc};
+            if(temp[1].second > sc){
+                temp[1] = {st ,sc};
             }
         }
-        dp.swap(tmp);
+        
+        // store in DP.
+        dp = temp;
     }
 
-    cout << min(dp[0].second,dp[1].second) << '\n';
+    lli ans = min( dp[0].second ,dp[1].second );
+    display(ans);
 }
 
 int32_t main(){   
     cin.tie(0), cin.sync_with_stdio(0);
+
+    freopen( "C:\\Users\\dell\\Documents\\Practice\\codefest\\Round_4\\Problem_3\\ts2_input.txt", "r" , stdin);
+    freopen("ts2_output.txt", "w", stdout);
+
     int t=1;
     cin >> t;
 
